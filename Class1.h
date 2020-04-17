c1b51d6ef92b7ffe08db7802d92dc4447216fae3
tiberiy99ak <tiberi.ak@yandex.ru>
2020-04-17 22:52:45 +0700

    2 ver of my project

--- Class1.h
+++ Class1.h
@@ -1,30 +1,20 @@
 #pragma once
 #include <iostream>
 #include <string>
-/*!
-	\brief Class output information
-	\author Im
-	\version 1.0
-	\date 10.04.2020
-*/
+
 using namespace std;
 class Class1
 {
 private:
 	string str;
 public:
-	/*!
-	Set value string
-	\param input Info massage
-	*/
+	
 	void SetString(string input)
 	{
 		str = input;
 
 	}
-	/*!
-	Output string on the screen
-	*/
+	
 	void writestring()
 	{
 		cout<<str;
--- test.cpp
+++ test.cpp
@@ -2,15 +2,11 @@
 #include "Class1.h"
 #include "Class2.h"
 #include "Class3.h"
-/*!
-	\brief Base File
-	\author Im
-	\version 1.0
-	\date 10.04.2020
-*/
+
 using namespace std;
 int main()
 {
+	getchar();
 	Class1 *_class = new Class1;
 	_class->SetString("Input value\n");
 	_class->writestring();
